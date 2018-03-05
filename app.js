68ed2fe0954e21de261c9edc1712dad9a80cca12
vukanmrvaljevic <31517206+vukanmrvaljevic@users.noreply.github.com>
2018-03-05 18:55:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -19,92 +19,76 @@ const rightButton = [
     document.getElementById('rightBtn8')
 ];
 
-var gradient = [
-    "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%), linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%)",
-    "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 100%), linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 100%)"
-];
-
-var img = [
-    "url()",
-    "url()",
-    "url()",
-    "url()",
-    "url()",
-    "url()",
-    "url()"
-];
-
-document.body.style.backgroundImage = img[0] + ", " + gradient[0];
 
 // Phase 1
 leftButton[0].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('questionTwo').style.display = "block";
-    document.getElementById('questionOne').style.display = "none";
-    document.body.style.backgroundImage = img[1] + ", " + gradient[1];  
+    document.getElementById('Two').style.display = "block";
+    document.getElementById('One').style.display = "none";
+  
 };
 rightButton[0].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathOne').style.display = "block";
-    document.getElementById('questionOne').style.display = "none";
+    document.getElementById('lostOne').style.display = "block";
+    document.getElementById('One').style.display = "none";
 };
 // Phase 2
 leftButton[1].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathTwo').style.display = "block";
-    document.getElementById('questionTwo').style.display = "none";
+    document.getElementById('lostTwo').style.display = "block";
+    document.getElementById('Two').style.display = "none";
 };
 rightButton[1].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('questionThree').style.display = "block";
-    document.getElementById('questionTwo').style.display = "none";
-    document.body.style.backgroundImage = img[2] + ", " + gradient[1];
+    document.getElementById('Three').style.display = "block";
+    document.getElementById('Two').style.display = "none";
+    
 };
 // Phase 3
 leftButton[2].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathThree').style.display = "block";
-    document.getElementById('questionThree').style.display = "none";
+    document.getElementById('lostThree').style.display = "block";
+    document.getElementById('Three').style.display = "none";
 };
 rightButton[2].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('questionFive').style.display = "block";
-    document.getElementById('questionThree').style.display = "none";
-    document.body.style.backgroundImage = img[3] + ", " + gradient[1];
+    document.getElementById('Five').style.display = "block";
+    document.getElementById('Three').style.display = "none";
+ 
 };
 // Phase 4
 leftButton[3].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('questionFive').style.display = "block";
-    document.getElementById('questionFour').style.display = "none";
+    document.getElementById('Five').style.display = "block";
+    document.getElementById('Four').style.display = "none";
 };
 rightButton[3].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathFour').style.display = "block";
-    document.getElementById('questionFour').style.display = "none";
+    document.getElementById('lostFour').style.display = "block";
+    document.getElementById('Four').style.display = "none";
 };
 
 // Phase 5
 leftButton[4].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathFive').style.display = "block";
-    document.getElementById('questionFive').style.display = "none";
+    document.getElementById('lostFive').style.display = "block";
+    document.getElementById('Five').style.display = "none";
 };
 rightButton[4].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('questionSix').style.display = "block";
-    document.getElementById('questionFive').style.display = "none";
-    document.body.style.backgroundImage = img[4] + ", " + gradient[0];
+    document.getElementById('Six').style.display = "block";
+    document.getElementById('Five').style.display = "none";
+
 };
 // Phase 6
 leftButton[5].onclick = function () {
     const answer = document.getElementById('buttons').value;
     document.getElementById('ending').style.display = "block";
-    document.getElementById('questionSix').style.display = "none";
-    document.body.style.backgroundImage = img[5] + ", " + gradient[0];
+    document.getElementById('Six').style.display = "none";
+  
 };
 rightButton[5].onclick = function () {
     const answer = document.getElementById('buttons').value;
-    document.getElementById('deathSix').style.display = "block";
-    document.getElementById('questionSix').style.display = "none";
+    document.getElementById('lostSix').style.display = "block";
+    document.getElementById('Six').style.display = "none";
 };
